0ba35b36141b9896a20f2abb37dd019c5878fa7b
ankit-ku-sahoo <10ankitkusahoo10@gmail.com>
2022-05-22 00:27:24 +0530

    Details showing up in webpage

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,20 @@
 import data from './data.js'
 
 console.log(data)
+const dataList = document.getElementById('namesContainer')
+const image = document.getElementById('image')
+const dataLength = data.length
+var counter = 0
+
+const loadData = () => {
+    data.map(item => {
+        var li = document.createElement("li")
+        li.appendChild(document.createTextNode(item.title))
+        dataList.appendChild(li)
+        console.log(item.title)
+    })
+
+    image.src = data[counter].previewImage
+}
+
+window.addEventListener('load', loadData)
